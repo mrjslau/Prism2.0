538f65c469f4ccfcbb9f2f6be75dc04d00962ab3
marijusiMac <marijuslau@yahoo.com>
2018-11-20 02:41:21 +0200

    Create Person, Identity, Location. Add PersonToIdentity

---
+++ prism2/db/migrate/20181120000957_create_identities.rb
@@ -0,0 +1,10 @@
+class CreateIdentities < ActiveRecord::Migration[5.1]
+  def change
+    create_table :identities, id: false, primary_key: :personal_code do |t|
+      t.integer :personal_code
+      t.string :full_name
+
+      t.timestamps
+    end
+  end
+end
---
+++ prism2/db/migrate/20181120003842_add_person_to_identities.rb
@@ -0,0 +1,5 @@
+class AddPersonToIdentities < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :identities, :person, foreign_key: true
+  end
+end
