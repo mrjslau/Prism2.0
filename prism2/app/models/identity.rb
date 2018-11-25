bd2d20db84c771a8a674b04a0b35a6fe9232911f
lakjustas <lakjustas@gmail.com>
2018-11-25 17:44:18 +0200

    Added Identity model

--- prism2/app/models/identity.rb
+++ prism2/app/models/identity.rb
@@ -1,4 +1,46 @@
 # Identity model, it has personal code as primary key
 class Identity < ApplicationRecord
   belongs_to :person
+
+  def name
+    full_name.fetch(:name)
+  end
+
+  def surname
+    full_name.fetch(:surname)
+  end
+
+  def add_criminal_record(offence, neighborhood)
+    criminal_records << { offence_level: offence,
+                          neighborhood: neighborhood }
+  end
+
+  def criminal_records_no
+    criminal_records.length
+  end
+
+  def criminal_status
+    if criminal_records_no > 0
+      if criminal_records_no > 5
+        'dangerous'
+      else
+        'suspicious'
+      end
+    else
+      'normal'
+    end
+  end
+
+  def status_change_msg
+    "#{name} #{surname}'s criminal status changed to: #{criminal_status}!"
+  end
+
+  def add_residence(building)
+    @residence = building
+  end
+
+  def remove_residence
+    remove_instance_variable(:@residence)
+  end
+
 end
