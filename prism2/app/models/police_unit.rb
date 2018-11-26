1d846fc2a7f9b2a019a08a60350cf80ff65f3907
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-11-27 00:07:36 +0200

    Add police tests for 'travel_to'

--- prism2/app/models/police_unit.rb
+++ prism2/app/models/police_unit.rb
@@ -4,8 +4,8 @@ class PoliceUnit < ApplicationRecord
   belongs_to :neighborhood
 
   def travel_to(neighborhood)
-    self.neighborhood.police_units.delete(id) if self.neighborhood
+    self.neighborhood.unit_exited(self) if self.neighborhood
     self.neighborhood = neighborhood
-    self.neighborhood.police_units << self
+    self.neighborhood.unit_entered(self)
   end
 end
