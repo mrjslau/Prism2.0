384a86ccbd598c3ba6ad8f729a97f1c1adbbd036
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-11-26 23:57:33 +0200

    Add brigade tests for 'travel_to'

--- prism2/app/models/fire_brigade.rb
+++ prism2/app/models/fire_brigade.rb
@@ -4,8 +4,8 @@ class FireBrigade < ApplicationRecord
   belongs_to :neighborhood
 
   def travel_to(neighborhood)
-    self.neighborhood.fire_brigades.delete(id) if self.neighborhood
+    self.neighborhood.unit_exited(self) if self.neighborhood
     self.neighborhood = neighborhood
-    self.neighborhood.fire_brigades << self
+    self.neighborhood.unit_entered(self)
   end
 end
---
+++ prism2/spec/fire_brigade_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe FireBrigade, type: :model do
+  fixtures :fire_brigades, :neighborhoods
+  let(:brigade) { fire_brigades(:brigade) }
+  
+  describe '#travel_to' do
+    context "enters a neighborhood" do
+      it "and notifies the neighborhood it entered" do
+        neighborhood = mock_model(Neighborhood)
+			  expect(neighborhood).to receive(:unit_entered).with(brigade)
+			  brigade.travel_to(neighborhood)
+      end
+
+      it "and notifies the neighborhood it left" do
+        old_hood = brigade.neighborhood
+        new_hood = mock_model(Neighborhood)
+        allow(new_hood).to receive(:unit_entered) { brigade }
+        brigade.travel_to(new_hood)
+        expect(old_hood.active_objects.fetch(:units)).not_to include(brigade)
+      end
+
+      it "and doesn't notify a neighborhood if wasnt in one" do
+        brigade.neighborhood = nil
+        hood = mock_model(Neighborhood)
+        allow(hood).to receive(:unit_entered) { brigade }
+        brigade.travel_to(hood)
+        expect(brigade.neighborhood).not_to receive(:unit_exited)
+      end
+
+      it "and changes the current active neighborhood" do
+        new_hood = mock_model(Neighborhood)
+        allow(new_hood).to receive(:unit_entered) { brigade }
+        brigade.travel_to(new_hood)
+        expect(brigade.neighborhood).to equal(new_hood)
+      end
+    end
+  end
+end
