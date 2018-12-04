a2aae72fc56be2e6263861f8ec3d7a71dffaeee9
Marijus Laucevicius <marijuslau@yahoo.com>
2018-12-04 18:38:56 +0200

    Hide maps

--- prism2/app/controllers/users_controller.rb
+++ prism2/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Success!"
+      flash[:success] = 'Success!'
       redirect_to @user
     else
       render 'new'
