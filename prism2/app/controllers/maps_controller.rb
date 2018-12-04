c38565dbf53811d5a427c82b8243046dd4682262
AdrianaLeisyte <adriana.leisyte@gmail.com>
2018-12-04 18:15:03 +0200

    Map controller and view ~ 30%

---
+++ prism2/app/controllers/maps_controller.rb
@@ -0,0 +1,29 @@
+# Maps controller
+class MapsController < ApplicationController
+  def index
+    @maps = Map.all
+  end
+
+  def new; end
+
+  def create
+    map = Map.new(map_params)
+
+    if map.save
+      redirect_to '/maps'
+    else
+      flash[:errors] = map.errors.full_messages
+      redirect_to '/maps/new'
+    end
+  end
+
+  def show
+    @map = Map.find(params[:id])
+  end
+
+  private
+
+  def map_params
+    params.require(:map).permit(:city_name)
+  end
+end
--- prism2/config/routes.rb
+++ prism2/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'maps' => 'maps#index'
+  get 'maps/new' => 'maps#new'
+  post 'maps' => 'maps#create'
+  get 'maps/:id' => 'maps#show'
 end
